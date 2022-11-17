c34d2ffb23856ccdeaa93f5483620bdff7135ef5
Glebova <e.tulzakova@mail.ru>
2022-11-17 22:17:39 +0400

    Проверка условий добавлена

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Car audi = new Car("Audi", "A8", 3.0, "черныйй", 2020, "Германия");
         Car bmw = new Car("Bmw", "Z8", 3.0, "черный", 2021, "Германия");
         Car kia = new Car("Kia", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея");
-        Car hyundai = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
+        Car hyundai = new Car("Hyundai", "", 0 , "", -2016, "Южная Корея");
         lada.info();
         audi.info();
         bmw.info();
